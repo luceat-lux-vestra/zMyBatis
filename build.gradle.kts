5d9b9dc7be992f87cb65b61233f2218fe30aef0f
luceat-lux-vestra <heathkimdev@gmail.com>
2026-09-14 00:29:42 +0900

    test(source): run resolve helper characterization

--- build.gradle.kts
+++ build.gradle.kts
@@ -63,7 +63,7 @@ dependencies {
 intellijPlatform {
     buildSearchableOptions = true
     pluginConfiguration {
-        name = providers.gradleProperty("pluginName")
+        name = providers.gradleProperty("pluginName").get()
         version = effectivePluginVersion
 
         val changelog = project.changelog // local variable for configuration cache compatibility
@@ -129,6 +129,7 @@ val javaParserIndexTest = intellijPlatformTesting.testIde.register("javaParserIn
             includeTestsMatching("com.algorist.zMyBatis.JavaActionContextDiskFixtureTest")
             includeTestsMatching("com.algorist.zMyBatis.source.ActiveEditorSourceSnapshotAdapterProjectFixtureTest")
             includeTestsMatching("com.algorist.zMyBatis.source.DependentMapperSourceSnapshotAdapterProjectFixtureTest")
+            includeTestsMatching("com.algorist.zMyBatis.source.JavaAnnotationConstantResolveHelperProjectFixtureTest")
             includeTestsMatching("com.algorist.zMyBatis.source.JavaAnnotationSourceCaptureAdapterProjectFixtureTest")
             includeTestsMatching("com.algorist.zMyBatis.source.JavaAnnotationSourceCaptureAdapterAdversarialProjectFixtureTest")
         }
@@ -165,7 +166,7 @@ changelog {
     versionPrefix = ""
 }
 
-// Configure Gradle Kover Plugin.
+// Configure Kover Plugin.
 kover {
     reports {
         total {
@@ -194,6 +195,7 @@ tasks {
             excludeTestsMatching("com.algorist.zMyBatis.KotlinActionContextBoundaryTest")
             excludeTestsMatching("com.algorist.zMyBatis.source.ActiveEditorSourceSnapshotAdapterProjectFixtureTest")
             excludeTestsMatching("com.algorist.zMyBatis.source.DependentMapperSourceSnapshotAdapterProjectFixtureTest")
+            excludeTestsMatching("com.algorist.zMyBatis.source.JavaAnnotationConstantResolveHelperProjectFixtureTest")
             excludeTestsMatching("com.algorist.zMyBatis.source.JavaAnnotationSourceCaptureAdapterProjectFixtureTest")
             excludeTestsMatching("com.algorist.zMyBatis.source.JavaAnnotationSourceCaptureAdapterAdversarialProjectFixtureTest")
             excludeTestsMatching("com.algorist.zMyBatis.source.JavaAnnotationSourceCaptureKotlinBoundaryTest")
